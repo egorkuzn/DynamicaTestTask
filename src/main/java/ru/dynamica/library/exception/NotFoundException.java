44d523e369427a12e16db38bdc5451c80dc2042b
shift-lab-kuznetsov <e.kuznetsov1@g.nsu.ru>
2023-12-01 00:54:41 +0700

    book-service: добавил ошибку

--- src/main/java/ru/dynamica/library/exception/NotFoundException.java
+++ src/main/java/ru/dynamica/library/exception/NotFoundException.java
@@ -1,4 +1,7 @@
 package ru.dynamica.library.exception;
 
-public class NotFoundException {
+public class NotFoundException extends Exception{
+    public NotFoundException(String message) {
+        super(message);
+    }
 }
